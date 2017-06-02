68b8a9b11614beed997ddfcac23cf669ea6808dd
Justin Bishop <jubishop@gmail.com>
2017-06-02 12:29:48 -0700

    script all working.

--- jeopardy.rb
+++ jeopardy.rb
@@ -1,9 +1,24 @@
 require 'nokogiri'
 require 'open-uri'
+require 'sqlite3'
+
+class FalseClass; def to_i; 0 end end
+class TrueClass; def to_i; 1 end end
 
 class ScrapeException < Exception; end;
 
-1.upto(5654) { |game_id|
+db = SQLite3::Database.new "jeopardy.sqlite3"
+
+db.execute('delete from game')
+db.execute('delete from category')
+db.execute('delete from question')
+db.execute('delete from final_jeopardy')
+
+def strip_answer_tags(answer)
+  Nokogiri::HTML(answer).text.gsub('\\', '')
+end
+
+1.upto(5654) { |game_id| # 5654 episodes
   begin
     doc = Nokogiri::HTML(open("games/game_#{game_id}.html"))
 
@@ -15,18 +30,31 @@ class ScrapeException < Exception; end;
 
     game_date = Date.parse(title)
 
+    db.execute('INSERT INTO GAME VALUES (?, ?)',
+      game_id,
+      game_date.to_time.to_i
+    )
+
     rounds = doc.css('table.round') # 2, single and double
     raise ScrapeException, "#{game_id}: No rounds" if (rounds.empty?)
 
     value_multiplier = [1, 2] # single and double jeopardy
-    questions = Array.new # slurp up all questions
     rounds.each { |round| # single and double
       # calculate value
       values = [200, 400, 600, 800, 1000]
       multiplier = value_multiplier.shift
 
       # get all categories
-      categories = round.css('td.category_name').map { |td| td.text }
+      category_names = round.css('td.category_name').map { |td| td.text }
+      category_ids = category_names.map { |category|
+        db.execute('INSERT INTO CATEGORY VALUES (null, ?, ?, ?)',
+          game_id,
+          category,
+          multiplier # round (1 or 2)
+        )
+        db.last_insert_row_id
+      }
+      categories = Hash[category_names.zip category_ids]
 
       # gather all questions
       clue_tds = round.css('td.clue')
@@ -37,30 +65,42 @@ class ScrapeException < Exception; end;
         next if (answer_div.empty?) # question never called
 
         # get the answer and the clue
-        answer = answer_div.first['onmouseover'].match('<em class="correct_response">(.+?)</em>').captures.first
+        answer = strip_answer_tags(
+          answer_div.first['onmouseover'].match('<em class="correct_response">(.+?)</em>').captures.first
+        )
         clue = clue_td.css('td.clue_text').first.text
 
         # detect if this clue is a daily double
         daily_double = !clue_td.css('td.clue_value_daily_double').empty?
 
-        questions.push({
-          :category => categories[clue_idx % 6],
-          :clue => clue,
-          :answer => answer,
-          :value => values[(clue_idx / 6).floor] * multiplier,
-          :round => (multiplier == 1) ? 'single' : 'double',
-          :daily_double => daily_double
-        })
+        # get our category id
+        category_id = categories[category_names[clue_idx % 6]]
+
+        db.execute('INSERT INTO QUESTION VALUES (null, ?, ?, ?, ?, ?, ?, ?)',
+          game_id,
+          category_id,
+          values[(clue_idx / 6).floor] * multiplier, # value of question
+          multiplier, # round (1 or 2)
+          daily_double.to_i, # is it a daily double?
+          clue,
+          answer
+        )
       }
     }
 
     final_round_table = doc.css('table.final_round')
     unless (final_round_table.empty?)
       final_round_div = final_round_table.css('div')
-      final_round_answer = final_round_div.first['onmouseover'].match(
+      final_round_answer = strip_answer_tags(final_round_div.first['onmouseover'].match(
         '<em class=\\\"correct_response\\\">(.+?)</em>'
-      ).captures.first
+      ).captures.first)
       final_round_clue = final_round_table.css('td.clue_text').text
+
+      db.execute('INSERT INTO FINAL_JEOPARDY VALUES (?, ?, ?)',
+        game_id,
+        final_round_clue,
+        final_round_answer
+      )
     end
   rescue ScrapeException => exception
     puts exception.message
