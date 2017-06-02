bd02a950ce524aa0a2ce2206fa4cc5a34e49bab2
Justin Bishop <jubishop@gmail.com>
2017-06-02 09:31:44 -0700

    new db. getting ready to load into sqlite3

--- jeopardy.rb
+++ jeopardy.rb
@@ -1,51 +1,60 @@
 require 'nokogiri'
 require 'open-uri'
 
-doc = Nokogiri::HTML(open('games/game_3.html'))
+def scrape_game(doc)
+  error = doc.css('p.error')
+  if (not error.empty?)
+    puts error.text
+    return
+  end
 
-title = doc.css('#game_title').text
+  title = doc.css('#game_title').text
+  game_date = Date.parse(title)
 
-rounds = doc.css('table.round') # 2, single and double
-value_multiplier = [1, 2] # single and double jeopardy
+  rounds = doc.css('table.round') # 2, single and double
+  value_multiplier = [1, 2] # single and double jeopardy
 
-questions = Array.new
-rounds.each { |round| # single and double
-  # calculate value
-  values = [200, 400, 600, 800, 1000]
-  multiplier = value_multiplier.shift
+  questions = Array.new
+  rounds.each { |round| # single and double
+    # calculate value
+    values = [200, 400, 600, 800, 1000]
+    multiplier = value_multiplier.shift
 
-  # get all categories
-  categories = round.css('td.category_name').map { |td| td.text }
+    # get all categories
+    categories = round.css('td.category_name').map { |td| td.text }
 
-  # gather all questions
-  clue_tds = round.css('td.clue')
-  0.upto(clue_tds.length - 1) { |clue_idx|
-    clue_td = clue_tds[clue_idx]
+    # gather all questions
+    clue_tds = round.css('td.clue')
+    0.upto(clue_tds.length - 1) { |clue_idx|
+      clue_td = clue_tds[clue_idx]
 
-    answer_div = clue_td.css('div')
-    next if (answer_div.empty?) # question never called
+      answer_div = clue_td.css('div')
+      next if (answer_div.empty?) # question never called
 
-    # get the answer and the clue
-    answer = answer_div.first['onmouseover'].match('<em class="correct_response">(.+?)</em>').captures.first
-    clue = clue_td.css('td.clue_text').first.text
+      # get the answer and the clue
+      answer = answer_div.first['onmouseover'].match('<em class="correct_response">(.+?)</em>').captures.first
+      clue = clue_td.css('td.clue_text').first.text
 
-    # detect if this clue is a daily double
-    daily_double = !clue_td.css('td.clue_value_daily_double').empty?
+      # detect if this clue is a daily double
+      daily_double = !clue_td.css('td.clue_value_daily_double').empty?
 
-    questions.push({
-      :category => categories[clue_idx % 6],
-      :clue => clue,
-      :answer => answer,
-      :value => values[(clue_idx / 6).floor] * multiplier,
-      :round => (multiplier == 1) ? 'single' : 'double',
-      :daily_double => daily_double
-    })
+      questions.push({
+        :category => categories[clue_idx % 6],
+        :clue => clue,
+        :answer => answer,
+        :value => values[(clue_idx / 6).floor] * multiplier,
+        :round => (multiplier == 1) ? 'single' : 'double',
+        :daily_double => daily_double
+      })
+    }
   }
-}
 
-final_round_table = doc.css('table.final_round')
-final_round_div = final_round_table.css('div')
-final_round_answer = final_round_div.first['onmouseover'].match(
-  '<em class=\\\"correct_response\\\">(.+?)</em>'
-).captures.first
-final_round_clue = final_round_table.css('td.clue_text').text
+  final_round_table = doc.css('table.final_round')
+  final_round_div = final_round_table.css('div')
+  final_round_answer = final_round_div.first['onmouseover'].match(
+    '<em class=\\\"correct_response\\\">(.+?)</em>'
+  ).captures.first
+  final_round_clue = final_round_table.css('td.clue_text').text
+end
+
+scrape_game(Nokogiri::HTML(open('http://www.j-archive.com/showgame.php?game_id=24')))
