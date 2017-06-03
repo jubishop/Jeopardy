071f4589a9c8a12b81df0a065af024ef116e6adc
Justin Bishop <jubishop@gmail.com>
2017-06-03 14:08:23 -0700

    splitting the pdf generation into separate pdf's for round 1 and round 2

--- print_pdf.rb
+++ print_pdf.rb
@@ -11,10 +11,13 @@ def print_game(game_id)
   null, game_utc = *$db.execute('SELECT * FROM GAME WHERE ID = ?', game_id).first
   game_date = Time.at(game_utc).to_datetime
 
-  questions_by_category = Hash.new { |hash, key| hash[key] = Array.new }
+  questions_round1 = Hash.new { |hash, key| hash[key] = Array.new }
+  questions_round2 = Hash.new { |hash, key| hash[key] = Array.new }
+  category_ids = Hash.new
   questions = $db.execute('SELECT * FROM QUESTION WHERE GAME_ID = ?', game_id)
   questions.each { |id, game_id, category_id, value, round, daily_double, clue, answer|
-    questions_by_category[category_id].push({
+    category_ids[category_id] = true
+    (round == 1 ? questions_round1 : questions_round2)[category_id].push({
       :clue => clue,
       :answer => answer,
       :daily_double => daily_double.to_bool,
@@ -23,13 +26,16 @@ def print_game(game_id)
     })
   }
 
-  categories = $db.execute("SELECT * FROM CATEGORY WHERE ID IN (#{questions_by_category.keys.join(', ')})")
+  categories = $db.execute("SELECT * FROM CATEGORY WHERE ID IN (#{category_ids.keys.join(', ')})")
   categories_by_id = categories.map { |id, game_id, category, round|
     [id, {:name => category, :round => round}]
   }.to_h
 
-  Prawn::Document.generate("cards/game_#{game_id}_questions.pdf") do
-    font_families.update(
+  rnd1 = Prawn::Document.new
+  rnd2 = Prawn::Document.new
+
+  [rnd1, rnd2].each { |rnd|
+    rnd.font_families.update(
       'Chalkboard' => { :bold => 'fonts/Chalkboard-Bold.ttf'},
       'Courgette' => { :normal => 'fonts/Courgette-Regular.ttf' },
       'Helvetica Inserat' => { :normal => 'fonts/Helvetica Inserat LT.ttf' },
@@ -38,46 +44,50 @@ def print_game(game_id)
         :bold => 'fonts/Korinna Bold.ttf'
       }
     )
+  }
 
+  [[questions_round1, rnd1], [questions_round2, rnd2]].each { |questions_by_category, rnd|
     box_count = 0
     questions_by_category.each { |id, questions|
       next if (questions.length < 5)
 
       box_count += 1
+      rnd.start_new_page if box_count > 1 and box_count.odd?
+
       box_base_y = box_count.odd? ? 720 : 340
-      bounding_box([20, box_base_y], :width => 500, :height => 340) do
-        stroke_bounds
-        bounding_box([65, 330], :width => 420, :height => 325) do
-          fill_color '000000'
+      rnd.bounding_box([20, box_base_y], :width => 500, :height => 340) do
+        rnd.stroke_bounds
+        rnd.bounding_box([65, 330], :width => 420, :height => 325) do
+          rnd.fill_color '000000'
           questions.each { |question|
-            pad(15) {
-              stroke_ellipse [-32, cursor - 13], 20, 10
-              font 'Helvetica Inserat', :size => 10
-              value =
-              text_box question[:value].to_s,
-                :at => [-42, cursor - 9],
+            rnd.pad(15) {
+              rnd.stroke_ellipse [-32, rnd.cursor - 13], 20, 10
+              rnd.font 'Helvetica Inserat', :size => 10
+              rnd.text_box question[:value].to_s,
+                :at => [-42, rnd.cursor - 9],
                 :align => :center,
                 :width => 20
 
-              font 'ITC Korinna', :style => :bold, :size => 9
-              text question[:clue].upcase
+              rnd.font 'ITC Korinna', :style => :bold, :size => 9
+              rnd.text question[:clue].upcase
 
-              font 'Chalkboard', :style => :bold, :size => 10
-              text_box question[:answer].upcase, :at => [20, cursor - 1]
+              rnd.font 'Chalkboard', :style => :bold, :size => 10
+              rnd.text_box question[:answer].upcase, :at => [20, rnd.cursor - 1]
             }
           }
-          font 'Courgette', :size => 10
-          fill_color '666666'
-          text_box game_date.strftime('%B %-d, %Y'),
+          rnd.font 'Courgette', :size => 10
+          rnd.fill_color '666666'
+          rnd.text_box game_date.strftime('%B %-d, %Y'),
             :at => [20, 25],
             :width => 400,
             :align => :right
         end
       end
-
-      start_new_page if box_count.even?
     }
-  end
+  }
+
+  rnd1.render_file "cards/game_#{game_id}_round1.pdf"
+  rnd2.render_file "cards/game_#{game_id}_round2.pdf"
 end
 
 print_game(5654)
